4e9468aa1897157b2c9b8d5ecb28c1476d7c5e1f
sych74 <sych74@gmail.com>
2016-09-29 23:57:51 +0200

    Update addEndpoint.js

--- scripts/addEndpoint.js
+++ scripts/addEndpoint.js
@@ -6,9 +6,7 @@ import com.hivext.api.development.Scripting;
 var envName = '${env.envName}';
 
 var resp = jelastic.env.control.AddEndpoint(envName, session, nodeId, port, "TCP", "Minecraft Server");
-print(resp)
 if (!resp.result) return resp;
-return resp;
 
 var scripting =  hivext.local.exp.wrapRequest(new Scripting({
     serverUrl : "http://" + window.location.host.replace("app", "appstore") + "/",
@@ -21,11 +19,12 @@ return scripting.eval({
     manifest : {
         "jpsType" : "update",
         "application" : {
-			"id": "minecraft-server",
-			"name": "Minecraft Live Migration",
-			"success": {
-				"email": "Your Minecraft server has been successfully deployed. Please use the following address of the server to connect your Minecraft client: ${env.domain}:" + resp.object.publicPort
-			}
+		"id": "minecraft-server",
+		"name": "Minecraft Live Migration",
+		"success": {
+			"text": "Your Minecraft server has been successfully deployed. </br> Please use the following server address to connect your Minecraft client: <a href='tcp://${env.domain}:" + resp.object.publicPort + "'>${env.domain}:" + resp.object.publicPort + "</a>."
+			"email": "Your Minecraft server has been successfully deployed. </br> Please use the following server address to connect your Minecraft client: <a href='tcp://${env.domain}:" + resp.object.publicPort + "'>${env.domain}:" + resp.object.publicPort + "</a>."
 		}
+	}
     }
 });
