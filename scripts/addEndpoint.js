241bef4cfca84ff1aef9eab5359a223fd4b9d4d4
sych74 <sych74@gmail.com>
2016-09-30 00:10:13 +0200

    Update addEndpoint.js

--- scripts/addEndpoint.js
+++ scripts/addEndpoint.js
@@ -19,8 +19,8 @@ return scripting.eval({
     manifest : {
         "jpsType" : "update",
         "application" : {
-		"id": "minecraft-server",
-		"name": "Minecraft Live Migration",
+		"id": "sendEmail",
+		"name": "Minecraft Server Address",
 		"success": {
 			"text": "Your Minecraft server has been successfully deployed. </br> Please use the following server address to connect your Minecraft client: <a href='tcp://${env.domain}:" + resp.object.publicPort + "'>${env.domain}:" + resp.object.publicPort + "</a>.",
 			"email": "Your Minecraft server has been successfully deployed. </br> Please use the following server address to connect your Minecraft client: <a href='tcp://${env.domain}:" + resp.object.publicPort + "'>${env.domain}:" + resp.object.publicPort + "</a>."
