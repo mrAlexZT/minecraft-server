f06be140528dc17dd63bca1edabf2664dcefdd24
sych74 <sych74@gmail.com>
2016-09-30 00:17:59 +0200

    Update addEndpoint.js

--- scripts/addEndpoint.js
+++ scripts/addEndpoint.js
@@ -1,29 +1,74 @@
-//@auth
-//@req(nodeId, port)
-
-import com.hivext.api.development.Scripting;
-
-var envName = '${env.envName}';
-
-var resp = jelastic.env.control.AddEndpoint(envName, session, nodeId, port, "TCP", "Minecraft Server");
-if (!resp.result) return resp;
-
-var scripting =  hivext.local.exp.wrapRequest(new Scripting({
-    serverUrl : "http://" + window.location.host.replace("app", "appstore") + "/",
-    session : session
-}));
-
-return scripting.eval({
-    script : "InstallApp",
-    targetAppid : envName,
-    manifest : {
-        "jpsType" : "update",
-        "application" : {
-		"id": "sendEmail",
-		"name": "Minecraft Server Address",
-		"success": {
-			"email": "Your Minecraft server has been successfully deployed. </br> Please use the following server address to connect your Minecraft client: <a href='tcp://${env.domain}:" + resp.object.publicPort + "'>${env.domain}:" + resp.object.publicPort + "</a>."
-		}
-	}
-    }
-});
+{
+  "jpsVersion": 0.8,
+  "jpsType": "install",
+  "application": {
+    "id": "minecraft-server",
+    "name": "Minecraft Server",
+    "description": "Minecraft server allows players to play online or via a local area network with other people.",
+    "logo": "https://github.com/jelastic-jps/minecraft-server/raw/master/images/Minecraft.png",
+    "homepage": "https://github.com/jelastic-jps/minecraft-server",
+    "env": {
+      "topology": {
+        "nodes": [
+          {
+            "docker": {
+              "image": "itzg/minecraft-server",
+              "env": {
+                "EULA": "TRUE"
+              }
+            },
+            "cloudlets": 16,
+            "nodeGroup": "cp",
+            "displayName": "Minecraft"
+          }
+        ]
+      }
+    },
+    "license": {
+      "terms": {
+        "en": "I agree with <a href='https://account.mojang.com/documents/minecraft_eula' target='_blank'><u>terms of service</u></a>"
+      }
+    },
+    "onInstall": [
+      {
+        "call": [
+          "updateConfiguration",
+          "addEndpoint"
+        ]
+      },
+      {
+        "restartContainers": {
+          "nodeGroup": "cp"
+        }
+      }
+    ],
+    "procedures": [
+      {
+        "id": "updateConfiguration",
+        "onCall": {
+          "executeShellCommands": {
+            "commands": [
+              "echo \"eula=true\" > /data/eula.txt",
+              "sed  -i \"/usermod\\|groupmod/d\" /start",
+              "wget https://github.com/jelastic-jps/minecraft-server/raw/master/properties/server.properties -O /data/server.properties"
+            ],
+            "nodeGroup": "cp"
+          }
+        }
+      },
+      {
+        "id": "addEndpoint",
+        "onCall": {
+          "executeScript": {
+            "type": "js",
+            "script": "https://github.com/jelastic-jps/minecraft-server/raw/master/scripts/addEndpoint.js?1212",
+            "params": {
+              "nodeId": "${nodes.cp.first.id}",
+              "port": 25565
+            }
+          }
+        }
+      }
+    ]
+  }
+}
