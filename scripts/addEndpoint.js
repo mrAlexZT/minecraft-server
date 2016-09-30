543628fce04294e89897817b7a409968ded9dcd7
sych74 <sych74@gmail.com>
2016-09-30 07:31:34 +0200

    Update addEndpoint.js

--- scripts/addEndpoint.js
+++ scripts/addEndpoint.js
@@ -16,14 +16,15 @@ var scripting =  hivext.local.exp.wrapRequest(new Scripting({
 return scripting.eval({
     script : "InstallApp",
     targetAppid : envName,
-    manifest : {
+    manifest : toJSON({
         "jpsType" : "update",
         "application" : {
 		"id": "sendEmail",
 		"name": "Minecraft Server Address",
 		"success": {
+			"text": "bla",
 		        "email": "Your Minecraft server has been successfully deployed. </br> Please use the following server address to connect your Minecraft client: <a href='tcp://${env.domain}:" + resp.object.publicPort + "'>${env.domain}:" + resp.object.publicPort + "</a>."
 		}
 	}
-    }
+    })
 });
