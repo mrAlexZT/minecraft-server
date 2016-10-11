e275a7841449407448b442e1c32d92e71cd54142
sych74 <sych74@gmail.com>
2016-10-11 16:54:17 +0200

    Update addEndpoint.js

--- scripts/addEndpoint.js
+++ scripts/addEndpoint.js
@@ -8,11 +8,16 @@ var envName = '${env.envName}';
 var resp = jelastic.env.control.AddEndpoint(envName, session, nodeId, port, "TCP", "Minecraft Server");
 if (resp.result != 0) return resp;
 
+var url = "${env.domain}:" + resp.object.publicPort;
+
+resp = jelastic.env.file.ReplaceInBody(envName, session, path, "${ENDPOINT_URL}", url, nodeId);
+if (resp.result != 0) return resp;
+
 var scripting =  hivext.local.exp.wrapRequest(new Scripting({
     serverUrl : "http://" + window.location.host.replace("app", "appstore") + "/"
 }));
 
-var text = "Your Minecraft server has been successfully deployed. </br> Please use the following server address to connect your Minecraft client: <a href='tcp://${env.domain}:" + resp.object.publicPort + "'>${env.domain}:" + resp.object.publicPort + "</a>.";
+var text = "Your Minecraft server has been successfully deployed. </br> Please use the following server address to connect your Minecraft client: <a href='tcp://" + url + "'>" + url + "</a>.";
 resp = scripting.eval({
     script : "InstallApp",
     targetAppid : '${env.appid}',
