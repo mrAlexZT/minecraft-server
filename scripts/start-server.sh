ac0e7b2ebc04aa5a5d792db9fe559d6900c0c706
sych74 <sych74@gmail.com>
2016-11-01 18:40:18 +0200

    Create start-server.sh

---
+++ scripts/start-server.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+source /data/memoryConfig.sh
+
+GC_PERIOD=${GC_PERIOD:-300}
+GC_DEBUG=${GC_DEBUG:-0}
+
+[ "$VERT_SCALING" != "false" -a "$VERT_SCALING" != "0" ] && JAVA_OPTS="$JAVA_OPTS -javaagent:/data/jelastic-gc-agent.jar=period=$GC_PERIOD,debug=$GC_DEBUG"
+
+source /start
