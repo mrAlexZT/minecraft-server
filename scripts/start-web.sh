222517badce125f9f217e881b5fedbd17c8bd4f4
mrAlexZT <mr.AlexZT@gmail.com>
2017-11-16 23:18:05 +0200

    Update start-web.sh

--- scripts/start-web.sh
+++ scripts/start-web.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-sed -i '/cd \/data/a [ -f /data/web/index.html ] && { sed -i "s/VANILLA_VERSION/$VANILLA_VERSION/g" /data/web/index.html; pushd /data/web; screen -md python -m SimpleHTTPServer 8080 ; popd; }' /start-minecraft
+sed -i '/cd \/data/a [ -f /data/web/index.html ] && { sed -i "s/VANILLA_VERSION/$VANILLA_VERSION/g" /data/web/index.html; pushd /data/web; screen -md python -m SimpleHTTPServer 8080 ; popd; }' /start-server.sh
+
 
